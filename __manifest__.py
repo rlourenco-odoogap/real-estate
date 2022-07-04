9526a1657ff12909578d7052b2a0f9accb47f255
Rodrigo Lourenço <rlourenco@odoogap.com>
2022-07-04 12:27:46 +0100

    [ADD] Action, security and menu added for real.estate.property.type

--- __manifest__.py
+++ __manifest__.py
@@ -6,6 +6,7 @@
   'data': [
     'security/ir.model.access.csv',
     'views/real_estate_property_views.xml',
+    'views/real_estate_property_type_views.xml',
     'views/real_estate_menus.xml'
   ],
   'application': True,
