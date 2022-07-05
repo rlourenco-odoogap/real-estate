9784308cf674cc347fd972403471555f6f54c1ed
Rodrigo Lourenço <rlourenco@odoogap.com>
2022-07-05 11:34:52 +0100

    [ADD] Date deadline and validity added to offer with computed fields on both sides

--- models/property_offer.py
+++ models/property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 class PropertyOffer(models.Model):
   _name = 'real.estate.property.offer'
@@ -11,3 +11,15 @@ class PropertyOffer(models.Model):
   ], copy=False)
   partner_id = fields.Many2one('res.partner', string="Partner")
   property_id = fields.Many2one('real.estate.property', string="Property")
+  validity = fields.Integer(default=7, string="Validity (days)")
+  date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline', string="Deadline")
+
+  @api.depends('date_deadline', 'validity')
+  def _compute_date_deadline(self):
+    for record in self:
+      record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity)
+
+  @api.depends('date_deadline', 'validity')
+  def _inverse_date_deadline(self):
+    for record in self:
+      record.validity = (record.date_deadline - fields.Date.today()).days
