0817aae5b7eba017e1d7b62e5bda416d3c64d12e
Rodrigo Lourenço <rlourenco@odoogap.com>
2022-07-05 11:13:19 +0100

    Total area and best offer price added to property

--- models/property.py
+++ models/property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 class Property(models.Model):
   _name = 'real.estate.property'
@@ -35,3 +35,18 @@ class Property(models.Model):
   salesperson = fields.Many2one('res.users', default=lambda self: self.env.user, string="Salesman")
   tag_ids = fields.Many2many('real.estate.property.tag', string="Tags")
   offer_ids = fields.One2many('real.estate.property.offer', 'property_id', string="Offers")
+  total_area = fields.Integer(compute='_compute_total_area')
+  best_price = fields.Float(compute='_compute_best_price', string="Best Offer")
+
+  @api.depends('living_area', 'garden_area')
+  def _compute_total_area(self):
+    for record in self:
+      record.total_area = record.living_area + record.garden_area
+
+  @api.depends('offer_ids')
+  def _compute_best_price(self):
+    for record in self:
+      if len(record.offer_ids) > 0:
+        record.best_price = max(offer.price for offer in record.offer_ids)
+      else:
+        record.best_price = 0
